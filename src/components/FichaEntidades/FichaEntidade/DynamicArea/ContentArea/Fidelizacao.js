b86166760ff28f04f8d3fc82e43eef0708019354
zebedal <zebedal@bitbucket.org>
2021-09-15 15:48:33 +0100

    vamos testar a ligação ao Qlik

--- src/components/FichaEntidades/FichaEntidade/DynamicArea/ContentArea/Fidelizacao.js
+++ src/components/FichaEntidades/FichaEntidade/DynamicArea/ContentArea/Fidelizacao.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from './Fidelizacao.module.css'
 
 const Fidelizacao = props => (
-    <div>
-        <h1>Fidelizacao</h1>
+    <div className={styles.grid}>
+        <span className={styles.gridTitles}>VM</span>
+        <span className={styles.gridTitles}>DM</span>
+        <span className={styles.gridTitles}>Fixo</span>
+        <span className={styles.gridTitles}>IOT</span>
+        <span className={styles.gridValues}>XXX</span>
+        <span className={styles.gridValues}>--</span>
+        <span className={styles.gridValues}>XXX&nbsp;</span>
+        <span className={styles.gridValues}>XX%</span>
     </div>
 )
 
